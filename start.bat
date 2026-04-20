74e89637278a1b89b5aa821cfa9064e058b830f0
Lordmayur <mayurrr03@gmail.com>
2026-04-20 20:39:06 +0530

    Initial commit

---
+++ start.bat
@@ -0,0 +1,3 @@
+@echo off
+start cmd /k "cd /d C:\Users\mayur\OneDrive\Desktop\newsnex\News-app\server && node index.js"
+start cmd /k "cd /d C:\Users\mayur\OneDrive\Desktop\newsnex\News-app\client && npm run dev"
